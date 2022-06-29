ccb3ad1b318970c853656276282aad27d6c79296
YuriDazio <dazioy@gmail.com>
2022-06-29 18:44:19 -0300

    Primeira aula de JavaScript + contador

---
+++ JavaScript/Aula 1/assets/js/scripts.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment(){
+    currentNumber++;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    currentNumber--;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+}
