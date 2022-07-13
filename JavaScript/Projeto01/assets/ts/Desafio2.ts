9993e743f608fc16a932e28bff1806d8419b2136
Yuri Dazio <dazioy@gmail.com>
2022-07-13 15:19:27 -0300

    Desafios de projetos finalizados.
    Desafios de projetos finalizados.

---
+++ JavaScript/Projeto01/assets/ts/Desafio2.ts
@@ -0,0 +1,25 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1: { nome: string, idade: number, profissao: string}={
+    nome:'maria',
+    idade:  29,
+    profissao: 'atriz'
+};
+
+let pessoa2: {nome: string, idade: number, profissao: string} = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "padeiro"
+};
+
+let pessoa3: {nome: string, idade: number, profissao: string} = {
+    nome: "laura",
+    idade: 32,
+    profissao: "atriz"
+};
+
+let pessoa4: {nome: string, idade: number, profissao: string} = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+};
